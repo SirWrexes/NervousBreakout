86a85f00db9f8bbace03dba37e500aef58838c9d
Ludovic Fernandez <ludofernandez@msn.com>
2025-05-11 01:45:27 +0200

    feat(Util.Colours.hexToRgb): Add support for alpha

--- game/lib/Util/Colours/hexToRgb.lua
+++ game/lib/Util/Colours/hexToRgb.lua
@@ -1,38 +1,49 @@
-local DIGIT = "%x"
-local HEX6 = DIGIT:rep(6)
-local HEX3 = DIGIT:rep(3)
-local HEX2 = DIGIT:rep(2)
-
 ---@param hex string Colour in hexadecimal format
 ---@return integer red
 ---@return integer green
 ---@return integer blue
+---@return integer alpha
 return function(hex)
     local type = type(hex)
-    assert(type == "string", ("Invalid value. Expected string, got %s."):format(type))
+    if type ~= string then error(("Invalid value. Expected string, got %s."):format(type), 2) end
+
+    hex = hex:gsub("^#", ""):match "^%x$"
+    if not hex then
+        error(("Invalid hexadecimal sequence: `%s`."):format(tostring(hex)))
+    elseif hex == "" then
+        error(("Invalid hexadecimal sequence: `%s`."):format(tostring(hex)))
+    end
 
     local red
     local green
     local blue
-    hex = hex:gsub("^#", "")
+    local alpha
 
-    if hex:match(HEX6) then
-        goto STRING_IS_HEX6
-    elseif hex:match(HEX3) then
+    local len = #hex
+    if len == 8 or len == 6 then
+        goto SEQUENCE_IS_COMPLETE
+    elseif len == 3 then
         red = hex:sub(1, 1):rep(2)
         green = hex:sub(2, 2):rep(2)
         blue = hex:sub(3, 3):rep(2)
         hex = ("%s%s%s"):format(red, green, blue)
-    elseif hex:match(HEX2) then
+    elseif len == 2 then
         hex = hex:rep(3)
     else
-        error(([[Invalid hex colour string: "%s".]]):format(hex))
+        error(
+            ("Hexadeximal colour string should be 2, 3, 6 or 8 characters long\nGot %d (`%s`)."):format(
+                len,
+                hex
+            )
+        )
     end
 
-    ::STRING_IS_HEX6::
+    ::SEQUENCE_IS_COMPLETE::
+    if len < 8 then hex = hex .. "ff" end
     red = tonumber(hex:sub(1, 2), 16)
     green = tonumber(hex:sub(3, 4), 16)
     blue = tonumber(hex:sub(5, 6), 16)
+    alpha = tonumber(hex:sub(7, 8), 16)
 
-    return red, green, blue
+    return red, green, blue, alpha
 end
