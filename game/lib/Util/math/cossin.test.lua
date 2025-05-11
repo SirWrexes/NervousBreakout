77b9c89f0b1d64ade3147b3079ef493a0db7eb18
Ludovic Fernandez <ludofernandez@msn.com>
2025-05-11 23:18:42 +0200

    test(lockmetatable & math.cossin): 100%

---
+++ game/lib/Util/math/cossin.test.lua
@@ -0,0 +1,17 @@
+require "lib.Util"
+
+describe("Util", function()
+    describe("math", function()
+        describe("cossin", function()
+            it("is consistent with math.cos() and math.sin()", function()
+                local sign = ({
+                    [1] = -1,
+                    [2] = 1,
+                })[math.random(2)]
+                local x = math.random() * sign
+
+                assert.same({ math.cos(x), math.sin(x) }, { math.cossin(x) })
+            end)
+        end)
+    end)
+end)
