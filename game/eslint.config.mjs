e862be739fa57c4867a9fc0651c39731469f61d5
Ludovic Fernandez <ludofernandez@msn.com>
2025-05-18 15:49:05 +0200

    style: Update (see below)
    - Import .prettierrc.json into ESLint config
    - Restrict tseslint.stylistic to JS-family sources
    - Modified some rules:
    ```js
    {
        '@stylistic/indent': 'off',
        '@stylistic/array-element-newline': ['error', 'consistent'],
        '@stylistic/array-bracket-newline': ['error', { multiline: true }],
        '@stylistic/spaced-comment': ['error', 'always', { exceptions: ['='] }],
        '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
    
        '@typescript-eslint/unified-signatures': 'off',
        '@typescript-eslint/no-unsafe-assignment': 'off',
    }
    ```

--- game/eslint.config.mjs
+++ game/eslint.config.mjs
@@ -3,54 +3,66 @@ import json from '@eslint/json'
 import markdown from '@eslint/markdown'
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 import prettier from 'eslint-plugin-prettier/recommended'
+import prettierrc from './.prettierrc.json' with { type: 'json' }
 
 export default defineConfig(
   /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
   [
     {
+      files: ['**/*.{js,mjs,cjs,ts}'],
+      plugins: { js },
+      extends: ['js/recommended'],
       languageOptions: {
         parserOptions: {
           projectService: true,
-          tsconfigRootDir: import.meta.dirname
-        }
-      }
-    },
-    {
-      files: ['**/*.{js,mjs,cjs,ts}'],
-      plugins: { js },
-      extends: ['js/recommended']
+          tsconfigRootDir: import.meta.dirname,
+        },
+      },
     },
     {
       files: ['**/*.json'],
       plugins: { json },
       language: 'json/jsonc',
-      extends: ['json/recommended']
+      extends: ['json/recommended'],
     },
     {
       files: ['**/*.md'],
       plugins: { markdown },
       language: 'markdown/gfm',
-      extends: ['markdown/recommended']
+      extends: ['markdown/recommended'],
     },
-    tseslint.configs.strictTypeChecked,
-    tseslint.configs.stylisticTypeChecked,
-
-    {
-      rules: {}
-    },
-
-    // Keep last to avoid any conflicts
+    stylistic.configs.customize({ jsx: false, braceStyle: '1tbs' }),
+    tseslint.configs.strictTypeChecked.map(conf => ({
+      ...conf,
+      files: ['**/*.{js,mjs,cjs,ts}'],
+    })),
     prettier,
     {
+      name: 'Formatting',
       rules: {
         'prettier/prettier': [
-          'off',
+          'warn',
+          prettierrc,
           {
-            endOfLine: 'auto'
-          }
-        ]
-      }
-    }
+            usePrettierrc: true,
+          },
+        ],
+      },
+    },
+    {
+      name: 'Overrides',
+      rules: {
+        '@stylistic/indent': 'off',
+        '@stylistic/array-element-newline': ['error', 'consistent'],
+        '@stylistic/array-bracket-newline': ['error', { multiline: true }],
+        '@stylistic/spaced-comment': ['error', 'always', { exceptions: ['='] }],
+        '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+
+        '@typescript-eslint/unified-signatures': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+      },
+    },
   ]
 )
