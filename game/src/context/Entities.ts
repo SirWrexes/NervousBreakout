cfb825212248339cdcd4e5d4fad80e14412e071d
Ludovic Fernandez <ludofernandez@msn.com>
2025-05-20 01:40:23 +0200

    feat(Context): Add Entities

---
+++ game/src/context/Entities.ts
@@ -0,0 +1,32 @@
+import type { Ball } from 'Ball'
+import type { Paddle } from 'Paddle'
+
+export class Entities {
+  private static _instance: Entities
+  private _paddle: Paddle
+  private _balls: Ball[]
+
+  private constructor(paddle: Paddle) {
+    this._paddle = paddle
+    this._balls = []
+  }
+
+  static init(paddle: Paddle) {
+    assert(!Entities._instance, 'Entities is already initialised')
+    Entities._instance = new Entities(paddle)
+  }
+
+  static update(dt: number) {
+    Entities._instance._paddle.update(dt)
+    Entities._instance._balls.forEach(ball => {
+      ball.update(dt)
+    })
+  }
+
+  static draw() {
+    Entities._instance._paddle.draw()
+    Entities._instance._balls.forEach(ball => {
+      ball.draw()
+    })
+  }
+}
