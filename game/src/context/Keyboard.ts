2e1bae85126007c00d364be48a8cb74c8ce46f94
Ludovic Fernandez <ludofernandez@msn.com>
2025-05-20 01:38:48 +0200

    feat(Context): Add Keyboard

---
+++ game/src/context/Keyboard.ts
@@ -0,0 +1,31 @@
+import { InputState } from './Input'
+import type { KeyConstant } from 'love.keyboard'
+
+export class Keyboard {
+  private static _instance: Keyboard
+  private _keys: Partial<Record<KeyConstant, InputState>>
+
+  private constructor() {
+    this._keys = {}
+  }
+
+  static init() {
+    assert(!Keyboard._instance, 'Mouse is already initialised')
+    Keyboard._instance = new Keyboard()
+  }
+
+  static update() {}
+
+  static button(key: KeyConstant) {
+    const down = love.keyboard.isDown(key)
+
+    switch (true) {
+      case down:
+        return (Keyboard._instance._keys[key] = InputState.DOWN)
+      case Keyboard._instance._keys[key] === InputState.DOWN:
+        return (Keyboard._instance._keys[key] = InputState.RELEASED)
+      default:
+        return (Keyboard._instance._keys[key] = InputState.UP)
+    }
+  }
+}
