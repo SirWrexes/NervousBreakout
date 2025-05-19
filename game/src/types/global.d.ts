8b6c1731c7aaa5a62d1e6045309281614118df10
Ludovic Fernandez <ludofernandez@msn.com>
2025-05-19 03:46:50 +0200

    refactor(types/global): Avoid namespace usage

--- game/src/types/global.d.ts
+++ game/src/types/global.d.ts
@@ -3,7 +3,25 @@
  */
 declare const arg: string[]
 
-declare global {
-  const __DEV: boolean
-  const __DEBUG: boolean
-}
+/**
+ * `true` if dev mode on
+ */
+declare const __DEV: boolean
+
+/**
+ * `true` if debug mode on
+ */
+declare const __DEBUG: boolean
+
+/**
+ * Types returned by the lua function `type(v)`
+ */
+declare type LuaTypeName =
+  | 'nil'
+  | 'number'
+  | 'string'
+  | 'boolean'
+  | 'table'
+  | 'function'
+  | 'thread'
+  | 'userdata'
